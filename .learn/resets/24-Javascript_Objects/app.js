6beb5d2a89df04caa66a1f75f68cac5a85443893
David Suarez <dsuarez001@att.net>
2022-09-19 00:55:44 +0000

    partial commit

--- .learn/resets/24-Javascript_Objects/app.js
+++ .learn/resets/24-Javascript_Objects/app.js
@@ -32,7 +32,7 @@ function addAllFamilyLuckyNumbers(anArray){
 }
 
 //Enter all your code here:
-person.lucky_numbers[3] = 33;
+
 
 //Do not make changes below:
 console.log(addAllFamilyLuckyNumbers(family.members)); 
